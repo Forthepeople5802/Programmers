d66736f0da07f782d1a7c35f814dbf3b4835308c
Forthepeople5802 <comet-1004@hanmail.net>
2023-07-05 02:02:15 +0900

    map

--- LEVEL1/달리기경주.java
+++ LEVEL1/달리기경주.java
@@ -1,11 +1,13 @@
 package LEVEL1;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
-// 시간복잡도 고민해봐야함...
+
 public class 달리기경주 {
     
     public static void main(String[] args) {
@@ -18,33 +20,49 @@ public class 달리기경주 {
         System.out.println(Arrays.toString(answer));
     }
 
+    // Array 탐색.. 시간 초과
+    // map으로 변경
     static class Solution {
-    public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
-          List<String> pList = Arrays.asList(players);
-        // ArrayList<String> pList = new ArrayList<>(Arrays.asList(players));
+        public String[] solution(String[] players, String[] callings) {
+            String[] answer = {};
+            Map<String, Integer> pMap = new HashMap<>();
+            for (int i = 0; i < players.length; i++) {
+                pMap.put(players[i], i);
+            }
+
             for (int i = 0; i < callings.length; i++) {
-                           
-                String  name = callings[i];
-                int idx = IntStream.range(0, pList.size())
-                        .filter(j -> Objects.equals(pList.get(j), name))
-                        .findFirst()
-                        .orElse(-1);
+                int idx = pMap.get(callings[i]);
+                String temp = players[idx-1];
+                players[idx-1] = players[idx];
+                players[idx] = temp;
+
+                pMap.put(callings[i], idx-1);
+                pMap.put(temp, pMap.get(temp)+1);
+
+            }
+
+            // ArrayList<String> pList = new ArrayList<>(Arrays.asList(players));
+            // for (int i = 0; i < callings.length; i++) {
+                            
+            //     String  name = callings[i];
+            //     int idx = IntStream.range(0, pList.size())
+            //             .filter(j -> Objects.equals(pList.get(j), name))
+            //             .findFirst()
+            //             .orElse(-1);
             
-                    if (idx > 0){
-                        String temp = players[idx -1];
-                        pList.set(idx - 1, callings[i]);
-                        pList.set(idx, temp);
-                        // pList.add(idx, callings[i]);
-                        // pList.remove(idx);
-                    }
+            //     if (idx > 0){
+            //         String temp = players[idx -1];
+            //         pList.set(idx - 1, callings[i]);
+            //         pList.set(idx, temp);
+            //         // pList.add(idx, callings[i]);
+            //         // pList.remove(idx);
+            //     }
                     
-                }
-            
-            
-            answer = pList.toArray(new String[pList.size()]);
-        return answer;
+            // }
+                
+                
+            return players;
+        }
     }
-}
 
 }
